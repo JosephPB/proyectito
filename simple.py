a725b86dad519ddb4929839511457e635dbd7b91
florpi <florpisoteadadejardin@gmail.com>
2018-04-30 10:55:21 +0100

    should work

--- simple.py
+++ simple.py
@@ -13,6 +13,8 @@ gray = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
 
 mask,contours,hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+print(mask)
+
 cnt = contours[-1]
 x,y,w,h =cv2.boundingRect(cnt)
 
